06c0fa14d96be7f1e186de2d06a9c0d02ee2d5ea
Revx0r <revxor@localhost.localdomain>
2019-08-27 17:11:27 -0700

    removed printu.c, and started code for Part2 myprintf

--- LAB1/P2_My_printf_Function/myprintf.c
+++ LAB1/P2_My_printf_Function/myprintf.c
@@ -2,6 +2,87 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+typedef unsigned int u32;
+
+char *ctable = "0123456789ABCDEF";
+int  BASE = 10; 
+
+int rpu(u32 x)
+{  
+    char c;
+    if (x){
+        c = ctable[x % BASE];
+        rpu(x / BASE);
+        putchar(c);
+    }
+}
+
+int printu(u32 x)
+{
+    //? If x==0 then run putchar('0'), else run rpu(x)
+    (x==0)? putchar('0') : rpu(x);
+    putchar(' ');
+}
+
+int printd(int x)
+{
+
+}
+
+int printx(u32 x)
+{
+    
+}
+
+int printo(u32 x)
+{
+    
+}
+
+
+
+int myprintf(char *fmt, ...)
+{
+    while(fmt) //* While is not null
+    {
+        if(fmt != '%')
+        {
+            putchar(fmt++);
+        }
+        else
+        {
+            fmt++; //Move up to the next character after %
+
+            switch ((int)fmt)
+            {
+            case 'c':   //? char
+                putchar(fmt);
+                break;
+            case 's':   //? string
+                /* code */
+                break;
+            case 'u':   //? unsigned integer
+                printu(fmt);
+                break;
+            case 'd':   //? integer
+                /* code */
+                break;
+            case 'o':   //? unsigned integer in OCT
+                /* code */
+                break;
+            case 'x':   //? unsigned integer in HEX
+                /* code */
+                break;
+
+            default:
+                break;
+            }
+        }
+        
+        
+    }
+}
+
 int main(int argc, char const *argv[])
 {
 /*     PART 2: myprintf Function
