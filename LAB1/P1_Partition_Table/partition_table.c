ebbe1b1d41898d5ace605148323874980e39e0fa
Revx0r <revxor@localhost.localdomain>
2019-08-28 15:12:50 -0700

    WIP!

--- LAB1/P1_Partition_Table/partition_table.c
+++ LAB1/P1_Partition_Table/partition_table.c
@@ -70,30 +70,72 @@ int main()
 		partition_number++; // Increment partition number
 	}
 
+	printf("********** Look for Extend Partition ************\n");
 	//! p=*p? EXTENDED Partition
-	//? p=&p; this assignment works... TODO:Possible fix/remove?
+	//? p=&p; this assignment works... TODO:remove?
 
-	lseek(fd, (long)(end_of_sector+1)*512, SEEK_SET);  // lseek to byte p4s start sector*512 OR in this case sector 1440
+	// lseek to byte (p4s start sector)*512 OR in this case sector 1440
+	//lseek(fd, (long)(p->start_sector)*512, SEEK_SET);
+	
+	//! Partition 5
+	lseek(fd, (long)1440*512, SEEK_SET);  
     read(fd, buf, 512);                  // read sector 123 into buf[ ]
 	p = &(buf[0x1BE]); // This should be the start of the first sector..
+	printf("%d   |  ", p->start_sector); //? 18
+	printf("%d \n", p->nr_sectors);		//? 342
+	p++;
+	printf("%d   |  ", p->start_sector); //? 377
+	printf("%d \n", p->nr_sectors); //? 523
+	
+
+	//! Partition 6
+	//int tempSector = p->start_sector + p->nr_sectors;
+	//tempSector = tempSector+1440;
+
+	//? 1440 + 523 = 1963 Xnot work
+	//?
+	lseek(fd, (long)1458*512, SEEK_SET);  //!this works but skipped a part
+    read(fd, buf, 512);                  // read sector 123 into buf[ ]
+	p = &(buf[0x1BE]); // This should be the start of the first sector..
+	printf("%d  |  ", p->start_sector);
+	printf("%d  |  \n", p->nr_sectors);
+
+
+	lseek(fd, (long)1817*512, SEEK_SET);  //!this works but skipped a part
+    read(fd, buf, 512);                  // read sector 123 into buf[ ]
+	p = &(buf[0x1BE]); // This should be the start of the first sector..
+	printf("%d  |  ", p->start_sector);
+	printf("%d  |  \n", p->nr_sectors);
+
+	//!Partition 7
+	//int tempSector = p->start_sector + p->nr_sectors;
+	//tempSector = tempSector+1440;
+
+	lseek(fd, (long)1452*512, SEEK_SET);  
+    read(fd, buf, 512);                  // read sector 123 into buf[ ]
+	p = &(buf[0x1BE]); // This should be the start of the first sector..
+	printf("%d  |  ", p->start_sector);
+	printf("%d  |  ", p->nr_sectors);
+	getchar();
+	
 	//! NOTES: The idea is that the P4's begin sector is a local MBR
 	//!        Thus, we should be able to access it like we had before?
 	//? Also if they are relative to P4's sector can't we just seek from there?
 	//? may be multiply the size of the partition *512 (size of sector)
 	//? To move on to the next partition...
 
-	while(p != 0)
+/* 	while(p != 0)
 	{
 		printf("%x", p->start_sector);
-		p++;
+		p+1440;
 		partition_number++; // Increment partition number
 		
 		//! FOR TESTING ONLY -REMOVE AFTER DEBUG
-		if (partition_number == 7)
+ 		if (partition_number == 7)
 		{
 			break;
 		}
-	}
+	} */
 
 
     //lseek(fd, (long)123*512, SEEK_SET);  // lseek to byte 123*512 OR sector 123
