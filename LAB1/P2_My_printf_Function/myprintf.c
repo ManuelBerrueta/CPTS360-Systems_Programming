61c02b9b97fbf4d5af0285bd51afbb8fd92ea39b
ManuelBerrueta <manuelberrueta@gmail.com>
2019-08-28 20:12:45 -0700

    Minor cleanup

--- LAB1/P2_My_printf_Function/myprintf.c
+++ LAB1/P2_My_printf_Function/myprintf.c
@@ -1,3 +1,8 @@
+/** 
+    Given: putchar(char c) of Linux, which prints a char.
+    2-1. Write YOUR own prints(char *s) fucntion to print a string.
+    Given: The following printu() function prints an unsignced integer. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -80,85 +85,15 @@ int prints(char* buf)
     }
 }
 
-
 int myprintf(char *fmt, ...)
 {
     int i = 0;
     int fmtSize = strlen(fmt);
-
-    //cp = (int*)fmt; //!worked
-    cp = fmt;
-    ip = fmt;
-    //printf("Address of cp: %8x", cp);
-    
-    //! Traversing up the stack to the parameters passed to myprintf
-    char *temp = cp;
-    int negcheck=0;
-
-
-    while(i < fmtSize) //* While is not null
-    {
-        //if(fmt[i] != '%')
-        //! Temp to check values
-        temp = cp;
-
-        if(*cp != '%')
-        {
-            //putchar(fmt[i++]);
-            putchar(*cp);
-            cp=cp++;
-            //printf(fmt);
-        }
-        else
-        {
-            //i++; //Move up to the next character after %
-            //! Decrenebt cp -2 up the stack to each parameter
-            ip = ip-2;
-
-            //switch ((int)fmt[++i])
-            i++;
-            // cp = cp+(i-1) ; //! Move to the next letter c,s,u,d,o - this worked
-            cp = cp++; //! Move to the next letter c,s,u,d,o
-            switch((int)*cp)
-            {
-            case 'c':   //? char
-                putchar(*ip-1);
-                putchar(' ');
-                break;
-            case 's':   //? string
-                /* code */
-                break;
-            case 'u':   //? unsigned integer
-                printu(*ip-1);
-                break;
-            case 'd':   //? integer
-                temp = (*ip-1);
-                break;
-            case 'o':   //? unsigned integer in OCT
-                printo(*ip-1);
-                break;
-            case 'x':   //? unsigned integer in HEX
-                printx(*ip-1);
-                break;
-
-            default:
-                break;
-            }
-        }
-    }
-    putchar('\n');
-    putchar('\r');
-}
-
-int notprintf(char *fmt, ...)
-{
-    int i = 0;
-    int fmtSize = strlen(fmt);
     int negcheck=0;
 
     //cp = (int*)fmt; //!worked
-    cp = fmt;
-    ip = &fmt; //* ip = the address of  fmt
+    cp = fmt;  //* cp is used to traverse the format string
+    ip = &fmt; //* ip = the address of fmt to traverse up the stack of the parameters passed
     
     //! Traversing up the stack to the parameters passed to myprintf
     char *temp = cp;
@@ -166,20 +101,16 @@ int notprintf(char *fmt, ...)
     while(i < fmtSize)
     {
         i++;
-        if (*cp == '\n')  //~ "\0"
+        if (*cp == '\n')
         {
             putchar('\n');
             putchar('\r');
             cp++;
-            //i++;
         }
         else if( *cp != '%')
         {
             putchar(*cp);
-            putchar('\n'); //! GDB won't print without this
-            //cp=cp+1; //? cp++ will not work....
-            cp++;
-            //i++;    
+            cp++;   
         }
         else
         {
@@ -193,8 +124,6 @@ int notprintf(char *fmt, ...)
                 cp++;
                 break;
             case 's':   //? string
-                /*TODO: Read that string until '\0' 
-                */
                 prints((char *)(*(++ip)));
                 cp++;
                 break;
@@ -230,23 +159,11 @@ int notprintf(char *fmt, ...)
 
 int main(int argc, char *argv[], char*env[])
 {
-/*     PART 2: myprintf Function
-    Given: putchar(char c) of Linux, which prints a char.
-    2-1. Write YOUR own prints(char *s) fucntion to print a string.
-    Given: The following printu() function prints an unsignced integer. */
-    //printf("testing\n");
-    //myprintf("T%xap", "1", "2", "3");
+    //!TESTER Code
 
-    //notprintf("Testing"); //!TESTER
+    myprintf("%u%c%x%o%d%s\n", 214, 'a', 100, 16, -1, "hello");
 
-    notprintf("%u%c%x%o%d%s\n", 214, 'a', 100, 16, -1, "hello");
-    //! String test
-    //notprintf("%s", "myStr");
-    //notprintf("%s%d", "Hello you are the: ", -1);
-    //myprintf("cha=%c string=%s      dec=%d hex=%x oct=%o neg=%d\n", 
-	//         'A', "this is a test", 100,    100,   100,  -100); 
-
-    // myprintf("cha=%c string=%s      dec=%d hex=%x oct=%o neg=%d\n", 
-	//         'A', "this is a test", 100,    100,   100,  -100);
+    myprintf("cha=%c string=%s      dec=%d hex=%x oct=%o neg=%d\n", 
+	          'A', "this is a test", 100,    100,   100,  -100); 
     return 0;
 }
