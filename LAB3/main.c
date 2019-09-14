30b62d0f34f7239169b06755a5a30ec8f545ed9c
Revx0r <revxor@localhost.localdomain>
2019-09-13 22:44:11 -0700

    Playing around with fork function

--- LAB3/main.c
+++ LAB3/main.c
@@ -1,6 +1,43 @@
 /******************************************************************************
- **                                                                          **
- **      She Simulator Programming Project - 2.13     **
+ **                Shell Simulator Programming Project - 3.11                **
  **                                 by                                       **
  **                           Manuel Berrueta                                **
  ******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+char command[16];       //? Command string
+char pathname[64];      //? Pathname string
+char dname[64];         //? Directory string holder
+char bname[64];         //? Basename string holder
+
+int main(int argc, char *argv[], char *env[])
+{
+    int i=0;
+    char buff[256] = "\n";
+    
+    while(i <= 2)
+    {
+        printf("| [Date] Berr Shell [cwd: s]\n-:> ");
+        scanf("%s", buff);
+        int pid = fork();
+        printf("%d\n", pid);
+        printf("getpid: %d\n",getpid());
+        i++;
+    }
+    
+    
+
+
+    //! Print Environment Vars
+    printf("*env[]: ");
+    while (env[i] != '\0')
+    {
+        printf("%s\n", env[i]); //! Prints all the Env Vars
+        //if (strcmp(env[i], ))
+        i++;
+    }
+
+    return 0;
+}
