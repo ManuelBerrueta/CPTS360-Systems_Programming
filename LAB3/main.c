b57072283d9b1011f26f0a43c2d569260f222db3
ManuelBerrueta <manuelberrueta@gmail.com>
2019-09-15 23:21:24 -0700

    Working with full path

--- LAB3/main.c
+++ LAB3/main.c
@@ -11,12 +11,11 @@
 #include <time.h>
 
 char command[16];       //? Command string
-char pathname[64];      //? Pathname string
 char dname[64];         //? Directory string holder
 char bname[64];         //? Basename string holder
 char cwd[256];
-char buff[256] = "\0";
-char *myargv[246] = "\0";
+char buff[256] = { 0 };
+char *myargv[246] = { 0 };
 int myargc = 0;
 
 int main(int argc, char *argv[], char *env[])
@@ -34,19 +33,89 @@ int main(int argc, char *argv[], char *env[])
     int i=0;
     int argcounter=0;
     char tempArg[64] = "\0";
+
+    //! Print env vars
+/*     while (env[i] != '\0')
+    {
+        printf("%s\n", env[i]);
+        i++;
+    } */
+
+    //! get the path
+    const char *path = getenv("PATH");
+    printf("PATH: %s\n", path);
+
     
-    while(i <= 2)
+    while(1)
     {
         printf("[ %04d/%02d/%02d ] BERR Shell [ %s ]\n|-$ ", tm.tm_year+1900, tm.tm_mon, tm.tm_mday, cwd);
-        scanf("%s", buff);
+        fgets(buff, sizeof(buff), stdin);
+        buff[strlen(buff)-1] = 0;
 
         //! (2) Handle simple commands
-        strcpy(command, strtok(buff, " "));
-        while(buff)
+        while(buff[i] != '\0')
         {
-            *tempArg = strtok(NULL, " ");
+            if(buff[i] == ' ')
+            { 
+                argcounter++;
+            }
             i++;
         }
+
+        i=0;
+        //! Tokenize command and parameters
+        strcpy(command, strtok(buff, " "));
+        myargv[i] = command;
+        i++;
+        if(argcounter > 0)
+        {
+            while(i < argcounter)
+            {
+                myargv[i] = strtok(NULL, " ");
+                i++;
+            }
+            i=0;
+        }
+
+
+        argcounter=0;
+        //! Tokenize Paths
+        while(path[i] != '\0')
+        {
+            if(path[i] == ':')
+            { 
+                argcounter++;
+            }
+            i++;
+        }
+        i=0;
+
+
+        //! Working forking child process!
+        int status;
+        int pid = fork();
+        if (pid)
+        {
+            pid=wait(&status);
+        }
+        else
+        {
+            //int r = execve(command, myargv, env);
+            char *tempPath = strtok(path, ":");
+            strcat(tempPath, command);
+            while (execve(command, myargv, env) == -1)
+            {
+                tempPath = strtok(NULL, ":");
+                strcat(tempPath, command);
+            }
+            //int r = execlp(command, myargv);
+            sleep(2);
+        }
+        
+
+        
+
+        //memset(myargv,0,sizeof(myargv));
         
         //TODO: Parse buff strok " "
         //TODO: First arg will be the command
@@ -54,41 +123,13 @@ int main(int argc, char *argv[], char *env[])
         //TODO: Last paramemter will be the target
         //TODO: Count number of arguments
         //TODO: Uset getopt(myargc, myargv.)
-        
-        
-        
-        
-        i++;
     }
 
-
-    
-    
-    
     //! PID Stuff
 /*     int pid = fork();
     printf("pid=fork: %d\n", pid);
     printf("getpid: %d\n, getppid: %d\n",getpid()), getppid();
  */
 
-    //! Print Environment Vars
-    printf("*env[]: ");
-    while (env[i] != '\0')
-    {
-        printf("%s\n", env[i]); //! Prints all the Env Vars
-        //if (strcmp(env[i], ))
-        i++;
-    }
-
     return 0;
-}
-
-int dbname(char *pathname)
-{
-    //! dirname() and basename() from libgen.h destroy the pathname!
-    char temp[128];
-    strcpy(temp, pathname); //* Make a copy of the path name
-    strcpy(dname, dirname(temp));
-    strcpy(temp, pathname);
-    strcpy(bname, basename(temp));
 }
